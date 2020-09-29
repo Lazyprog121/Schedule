890d3b1f987f4fe1b68c82f96819b6004f3a332d
Kotiai Danylo <danilo.anatolievich@gmail.com>
2020-09-29 17:09:14 +0300

    commit4

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KPI_Schedule.Models;
+
+namespace KPI_Schedule.Controllers
+{
+    public class HomeController : Controller
+    {
+        public DbSchedule Context { get; }
+        public HomeController(DbSchedule context)
+        {
+            Context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,26 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using KPI_Schedule.Models;
 
 namespace KPI_Schedule
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbSchedule>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -24,10 +35,7 @@ namespace KPI_Schedule
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=home}/{action=start}");
             });
         }
     }
